ae26335544a4ed7d78d2c3205dccfb5469f96700
KpryKorn <roffinisacha@gmail.com>
2023-05-21 11:38:43 +0200

    started working on the UI

---
+++ src/front/screen/ColorSelector.tsx
@@ -0,0 +1,40 @@
+import { Player, PlayerColor } from "../../types";
+
+type ColorSelectorProps = {
+  onSelect: (color: PlayerColor) => void;
+  players: Player[];
+  colors: PlayerColor[];
+};
+
+function discColor(color: PlayerColor) {
+  return `disc disc-${color === PlayerColor.YELLOW ? "yellow" : "red"}`;
+}
+
+export function ColorSelector({
+  onSelect,
+  players,
+  colors,
+}: ColorSelectorProps) {
+  return (
+    <>
+      <div className="players">
+        {players.map((player) => (
+          <div key={player.id} className="player">
+            {player.name}
+            {player.color && <div className={discColor(player.color)} />}
+          </div>
+        ))}
+      </div>
+      <h3>Sélectionnez une couleur</h3>
+      <div className="selector">
+        {colors.map((color) => (
+          <button
+            className={discColor(color)}
+            key={color}
+            onClick={() => onSelect(color)}
+          ></button>
+        ))}
+      </div>
+    </>
+  );
+}
